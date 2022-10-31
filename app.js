9c2a9fe83e3f0dd68daa70646e13f3f8ba710de5
loula <wmhadjou@gmail.com>
2022-10-31 15:39:35 +0100

    add file for database

---
+++ app.js
@@ -0,0 +1,2 @@
+//app.js est l'interface ( ce qu'on voit ) il sera lié au server.js
+//app.js est un constructeur avec des propriétés qu'on developpera dans les autres classes
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-//console.log("hello world");
+//creation et lecture de http par node
 const http = require("http");
 
+//creation du serveur avec une requete et la reponse attendue
 const server = http.createServer((req, res) => {
-  res.end("Voilà la réponse du serveur !");
+  //l'equivalent d'un print
+  res.end("Ousmane mon prince !");
 });
 
+//affichage sur le port 8000 de l'equivalent du print
 server.listen(process.env.PORT || 8000);
