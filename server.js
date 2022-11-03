bd47fcbc9380a557ac68adf2e165c67ee055e926
Marielle K <marielle.koffi1@gmail.com>
2022-11-03 11:38:04 +0100

    conflict git

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const { products } = require("./data/products.js");
 const app = express();
@@ -6,62 +5,60 @@ const mongoose = require("mongoose");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 //to avoid CORS cross origin errors since we will use react and node server
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 //to be able pass json data
-app.use(express.json())
+app.use(express.json());
 //routes
-app.use("/api/auth",authRoute)
-app.use("/api/users",userRoute)
-app.use("/api/produits",productRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/produits", productRoute);
 
 //connect to DB
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(
-        () => console.log("DB connection established")
-    )
-    .catch(
-        err => console.log(err)
-    );
-
-
-
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection established"))
+  .catch((err) => console.log(err));
 
 //Load products from server
 app.get("/api/productss", (req, res) => {
-    res.json(products)
-})
+  res.json(products);
+});
 
-
-
-//parts of product from server 
+//parts of product from server
 app.get("/api/productz", (req, res) => {
-    const partsOfProduct = products.map((p) => {
-        const { id, name, image } = p
-        return { id, name, image }
-    })
-    res.json(partsOfProduct)
-})
+  const partsOfProduct = products.map((p) => {
+    const { id, name, image } = p;
+    return { id, name, image };
+  });
+  res.json(partsOfProduct);
+});
 
 //single product
 app.get("/api/productz/:id", (req, res) => {
-    const singleProduct = products.find((p) => p.id === req.params.id)
-    res.json(singleProduct)
-})
+  const singleProduct = products.find((p) => p.id === req.params.id);
+  res.json(singleProduct);
+});
 
 app.get("/", (req, res) => {
-    res.send("API is running ...")
-})
+  res.send("API is running ...");
+});
 
 //launching a server
 app.listen(process.env.PORT || 8000, () =>
-    console.log("server running on 8000..."));
+  console.log("server running on 8000...")
+);
